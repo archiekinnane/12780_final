96d1a0cf7b3970b12fd41311fedd96d449c98eea
archiekinnane <40501353+archiekinnane@users.noreply.github.com>
2025-12-14 15:09:39 -0500

    feat: added and tested routing to dashboard

--- weather_tracker/weather/views.py
+++ weather_tracker/weather/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def hello(request):
+   return HttpResponse("Hello World from Hyperlocal Weather Tracker App!")
+
+def dashboard(request):
+   return HttpResponse("Dashboard works")
