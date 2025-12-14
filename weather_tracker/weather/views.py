fad794c911af9ac6ca5ed2d3ea462df10de129e9
archiekinnane <40501353+archiekinnane@users.noreply.github.com>
2025-12-14 16:11:20 -0500

    feat: added placeholder dashboard page and confirmed data pipeline

--- weather_tracker/weather/views.py
+++ weather_tracker/weather/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from weather.models import WeatherQuery
+
 # Create your views here.
 def hello(request):
    return HttpResponse("Hello World from Hyperlocal Weather Tracker App!")
 
 def dashboard(request):
-   return HttpResponse("Dashboard works")
+   data = WeatherQuery.objects.all()
+   return render(request, "weather/dashboard.html", {"queries": data})
